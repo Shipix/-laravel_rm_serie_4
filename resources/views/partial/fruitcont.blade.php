97470ddbcdd8e9184c85c44892db348ee53b3fe3
Shipix <shipix@hotmail.fr>
2021-02-16 14:24:41 +0100

    PT-3

--- resources/views/partial/fruitcont.blade.php
+++ resources/views/partial/fruitcont.blade.php
@@ -1,10 +1,19 @@
 <div class="container d-flex">
-    @foreach ($fruitdata as $fruit)
-    <div class="card m-2" style="width: 18rem;">
+    @foreach ($fruitdata as $fruit)  
+    @if (Str::length($fruit->name)>=7)   
+    <div class="card m-2 bg-primary" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">{{$fruit->name}}</h5>
           <h6 class="card-subtitle mb-2 text-muted">{{$fruit->qantite}}</h6>
         </div>
     </div>
+    @else
+    <div class="card m-2 " style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">{{$fruit->name}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{$fruit->qantite}}</h6>
+      </div>
+  </div>
+    @endif
     @endforeach
 </div>
--- resources/views/partial/legumecont.blade.php
+++ resources/views/partial/legumecont.blade.php
@@ -1,10 +1,19 @@
 <div class="container d-flex">
     @foreach ($legumedata as $legume)
+    @if (Str::length($legume->name)>=7)
+    <div class="card m-2 bg-success" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">{{$legume->name}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{$legume->qantite}}</h6>
+      </div>
+  </div>       
+    @else
     <div class="card m-2" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title">{{$legume->name}}</h5>
           <h6 class="card-subtitle mb-2 text-muted">{{$legume->qantite}}</h6>
         </div>
     </div>
+    @endif
     @endforeach
 </div>
